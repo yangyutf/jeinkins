d35731724e4d46316e85f110efc03412c74c2f64
yangyutf <63225698+yangyutf@users.noreply.github.com>
2020-08-13 17:41:44 +0800

    Initial commit

---
+++ src/main/java/yy/cloud/jenkcom/JenkcomApplication.java
@@ -0,0 +1,13 @@
+package yy.cloud.jenkcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yy/cloud/jenkcom/JenkcomApplicationTests.java
@@ -0,0 +1,13 @@
+package yy.cloud.jenkcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkcomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
